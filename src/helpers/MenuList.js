4bc48480e26164dbc129ffab1c00d313da3a5366
Jkiptoo <jonnahkapkory15384@gmail.com>
2023-06-22 18:54:12 +0300

    Details needed for the menu

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from '../assets/pepperoni.jpg'
+import Margherita from '../assets/margherita.jpg'
+import MeekSteaK from '../assets/pedrotechspecial.jpg'
+import Vegan from '../assets/vegan.jpg'
+import Pineapple from '../assets/pineapple.jpg'
+import Expensive from '../assets/expensive.jpg'
+
+export const MenuList = [
+    {
+        name: 'Pepperoni Pizza',
+        image: Pepperoni,
+        price: 12.99
+    },
+    {
+        name: 'Margherita Pizza',
+        image: Margherita,
+        price: 14.99
+    },
+    {
+        name: 'MeekSteak Pizza',
+        image: MeekSteaK,
+        price: 17.99
+    },
+    {
+        name: 'Vegan Pizza',
+        image: Vegan,
+        price: 22.99
+    },
+     {
+        name: 'Pineapple Pizza',
+        image: Pineapple,
+        price: 7.99
+    },
+    {
+        name: 'Mushroom Pizza',
+        image: Expensive,
+        price: 69.99
+    }
+]
